105bb742768cd6ab0975c46775233760b1178810
Saif Uddin Mahmud <gzsaif001@gmail.com>
2020-09-25 13:44:41 +0800

    Make readability changes

--- conway.hs
+++ conway.hs
@@ -28,9 +28,9 @@ main = do
 
 -- simulate and print each step from t to t+n
 simulate n curState = do
-    let countNeighbours (x, y) mtx = foldr (\(i, j) acc -> if (mtx!(i, j))=='+' then acc+1 else acc) 0 [(x-1, y-1), (x-1, y), (x-1, y+1), (x, y-1), (x, y+1), (x+1, y-1), (x+1, y), (x+1, y+1)]
+    let countNeighbours (x, y) mtx = foldr (\(i, j) acc -> if (mtx!(i, j))==alive then acc+1 else acc) 0 [(x-1, y-1), (x-1, y), (x-1, y+1), (x, y-1), (x, y+1), (x+1, y-1), (x+1, y), (x+1, y+1)]
     let toBeBorn mtx = filter (\(x, y) -> if (countNeighbours (x, y) mtx)==3 then True else False) [(i, j) | i <- [1..((num_rows mtx)-1)], j <- [1..((num_cols mtx)-1)]]
-    let toDie mtx = filter (\(x, y) -> let count = countNeighbours (x, y) mtx in if mtx!(x,y)=='+' then (if count<2 then True else if count > 3 then True else False) else False) [(i, j) | i <- [1..((num_rows mtx)-1)], j <- [1..((num_cols mtx)-1)]]
+    let toDie mtx = filter (\(x, y) -> let count = countNeighbours (x, y) mtx in if mtx!(x,y)==alive then (if count<2 then True else if count > 3 then True else False) else False) [(i, j) | i <- [1..((num_rows mtx)-1)], j <- [1..((num_cols mtx)-1)]]
     let takeStep state = activateCells (toBeBorn state) $ deactivateCells (toDie state) state
 
     if n <= 0 then return curState
@@ -44,17 +44,19 @@ simulate n curState = do
 num_cols mtx = snd $ snd $ bounds mtx
 num_rows mtx = fst $ snd $ bounds mtx
 
+blank = ' '
+alive = '+'
+
 rowAsStr mtx x = [mtx ! (x, y) | y <- [0..(num_cols mtx)]]
 mtxAsStr mtx = unlines [rowAsStr mtx x | x <- [0..(num_rows mtx)]]
 printMtx mtx = putStr (mtxAsStr mtx)
 
-activateCells xs mtx = mtx // [(x, '+') | x <- xs]
-deactivateCells xs mtx = mtx // [(x, ' ') | x <- xs]
-
+activateCells xs mtx = mtx // [(x, alive) | x <- xs]
+deactivateCells xs mtx = mtx // [(x, blank) | x <- xs]
 
 initializeMtx n m set_cells =
     -- intialize blank state. Padding of length 1 on all sides
-    let emptyMtx = (array ((0, 0), (n+1, m+1)) [((i, j), ' ') | i <- [0..(n+1)], j <- [0..(m+1)]]) 
+    let emptyMtx = (array ((0, 0), (n+1, m+1)) [((i, j), blank) | i <- [0..(n+1)], j <- [0..(m+1)]]) 
     in
-      let createBorders mtx = mtx // ([((0, j), '-') | j <- [0..(num_cols mtx)]] ++ [((num_rows mtx, j), '-') | j <- [0..(num_cols mtx)]] ++ [((i, 0), '|') | i <- [0..(num_rows mtx)]] ++ [((i, (num_cols mtx)), '|') | i <- [0..(num_rows mtx)]])
+      let createBorders mtx = mtx // ([((0, j), '-') | j <- [0..(num_cols mtx)]] ++ [((num_rows mtx, j), '-') | j <- [0..(num_cols mtx)]] ++ [((i, 0), '|') | i <- [1..(num_rows mtx)-1]] ++ [((i, (num_cols mtx)), '|') | i <- [1..(num_rows mtx)-1]])
       in activateCells set_cells (createBorders emptyMtx)
