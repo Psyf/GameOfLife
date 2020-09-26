f511f97ed93676ae0acf6907d941f5a3e4b0f8a1
Saif Uddin Mahmud <gzsaif001@gmail.com>
2020-09-26 16:45:52 +0800

    unicodify for prettier visuals

--- conway.hs
+++ conway.hs
@@ -1,5 +1,6 @@
 import Data.Array
 import Control.Concurrent
+import System.Process
 
 main = do
 
@@ -13,7 +14,7 @@ main = do
     num_cols <- getLine 
     let y = (read num_cols :: Int)
 
-    putStrLn "Type a pattern name from (blinker, toad, beacon, pulsar, penta-decathlon, glider, lightweight-spaceship, middleweight-spaceship, heavyweight-spaceship, random)"
+    putStrLn "Type a pattern name from (blinker, toad, glider, r-pento, diehard)"
     pattern_str <- getLine
 
     let initState = initializeMtx x y (generatePattern pattern_str (x `div` 2) (y `div` 2))
@@ -34,7 +35,7 @@ main = do
 -- simulate and print each step from t to t+n
 simulate n curState delay = do
     let countNeighbours (x, y) mtx = foldr (\(i, j) acc -> if (mtx!(i, j))==alive then acc+1 else acc) 0 [(x-1, y-1), (x-1, y), (x-1, y+1), (x, y-1), (x, y+1), (x+1, y-1), (x+1, y), (x+1, y+1)]
-    let toBeBorn mtx = filter (\(x, y) -> if (mtx!(x,y)==blank && (countNeighbours (x, y) mtx)==3) then True else False) [(i, j) | i <- [1..((num_rows mtx)-1)], j <- [1..((num_cols mtx)-1)]]
+    let toBeBorn mtx = filter (\(x, y) -> if (mtx!(x,y)==dead && (countNeighbours (x, y) mtx)==3) then True else False) [(i, j) | i <- [1..((num_rows mtx)-1)], j <- [1..((num_cols mtx)-1)]]
     let toDie mtx = filter (\(x, y) -> let count = countNeighbours (x, y) mtx in if (mtx!(x,y)==alive && count /= 2 && count /= 3) then True else False) [(i, j) | i <- [1..((num_rows mtx)-1)], j <- [1..((num_cols mtx)-1)]]
     let takeStep state = activateCells (toBeBorn state) $ deactivateCells (toDie state) state
 
@@ -50,24 +51,28 @@ simulate n curState delay = do
 num_cols mtx = snd $ snd $ bounds mtx
 num_rows mtx = fst $ snd $ bounds mtx
 
-blank = ' '
-alive = '+'
+alive = '⬜'
+dead = '⬛'
+boundary = ' '
 
 rowAsStr mtx x = [mtx ! (x, y) | y <- [0..(num_cols mtx)]]
 mtxAsStr mtx = unlines [rowAsStr mtx x | x <- [0..(num_rows mtx)]]
 printMtx mtx = putStr (mtxAsStr mtx)
 
 activateCells xs mtx = mtx // [(x, alive) | x <- xs]
-deactivateCells xs mtx = mtx // [(x, blank) | x <- xs]
+deactivateCells xs mtx = mtx // [(x, dead) | x <- xs]
 
 initializeMtx n m set_cells =
-    -- intialize blank state. Padding of length 1 on all sides
-    let emptyMtx = (array ((0, 0), (n+1, m+1)) [((i, j), blank) | i <- [0..(n+1)], j <- [0..(m+1)]]) 
+    -- intialize dead state. Padding of length 1 on all sides
+    let emptyMtx = (array ((0, 0), (n+1, m+1)) [((i, j), dead) | i <- [0..(n+1)], j <- [0..(m+1)]]) 
     in
-      let createBorders mtx = mtx // ([((0, j), '-') | j <- [0..(num_cols mtx)]] ++ [((num_rows mtx, j), '-') | j <- [0..(num_cols mtx)]] ++ [((i, 0), '|') | i <- [1..(num_rows mtx)-1]] ++ [((i, (num_cols mtx)), '|') | i <- [1..(num_rows mtx)-1]])
+      let createBorders mtx = mtx // ([((0, j), boundary) | j <- [0..(num_cols mtx)]] ++ [((num_rows mtx, j), boundary) | j <- [0..(num_cols mtx)]] ++ [((i, 0), boundary) | i <- [1..(num_rows mtx)-1]] ++ [((i, (num_cols mtx)), boundary) | i <- [1..(num_rows mtx)-1]])
       in activateCells set_cells (createBorders emptyMtx)
 
 -- 1. TODO add more patterns
 -- 2. TODO allow custom inputs
 generatePattern "blinker" x y =  [(x, y-1), (x, y), (x, y+1)]
 generatePattern "toad" x y = [(x, y-1), (x, y), (x, y+1), (x+1, y), (x+1, y-1), (x+1, y-2)]
+generatePattern "glider" x y = [(x-1, y), (x, y), (x+1, y), (x+1, y-1), (x, y-2)]
+generatePattern "r-pento" x y = [(x, y), (x-1, y), (x+1, y), (x-1, y+1), (x, y-1)]
+generatePattern "diehard" x y = [(x, y), (x+1, y), (x, y-1), (x+1, y+4), (x+1, y+5), (x+1, y+6), (x-1, y+5)]
